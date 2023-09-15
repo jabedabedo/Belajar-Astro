4fc0d0d709445f4d4299c5d90bed5b80320d3619
root <root@IDC-OPS-SURYA.IDCLOUD.ID>
2023-09-15 14:18:06 +0700

    update navbar+hero

--- src/partials/Hero.tsx
+++ src/partials/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => (
     <HeroAvatar
       title={
         <>
-          Hi there, I'm <GradientText>Ixartz</GradientText> 👋
+          Hi there, I'm <GradientText>Jabar</GradientText> 👋
         </>
       }
       description={
