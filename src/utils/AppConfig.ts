9ce0cdc53e3e22b1d51c9690d0728716e8b466fa
root <root@IDC-OPS-SURYA.IDCLOUD.ID>
2023-09-15 15:19:59 +0700

    update appconfig

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,8 @@
 export const AppConfig = {
-  site_name: 'Astro boilerplate',
-  title: 'Astro boilerplate',
-  description: 'Boilerplate built with Astro using React and Tailwind CSS',
-  author: 'Emma',
-  locale_region: 'en-us',
-  locale: 'en',
+  site_name: 'Jabar Belajar Astro',
+  title: 'Jabar Belajar Astro',
+  description: 'Jabar built with Astro using Boilerplate, React, and Tailwind CSS',
+  author: 'Jabar',
+  locale_region: 'jkt-idn',
+  locale: 'jkt',
 };
